283ba74850ca16f9c0527b3038300faf376ee08a
Yoseph-M <zubease22@gmail.com>
2026-08-06 16:53:09 +0300

    chore: update jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'json'],
   globalSetup: '<rootDir>/tests/globalSetup.ts',
   globalTeardown: '<rootDir>/tests/globalTeardown.ts',
-  setupFilesAfterSetup: ['<rootDir>/tests/setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   testTimeout: 30000,
   // Ensure each test file gets a fresh module registry
   // so prisma re-connects to the in-memory DB
