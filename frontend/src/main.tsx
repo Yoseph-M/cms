5a9c1dafea5914e0ece298ec4eafc73dc79ea6d5
Yoseph-M <zubease22@gmail.com>
2026-08-12 23:53:50 +0300

    feat: update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
+import './i18n';
 import { ErrorBoundary } from './components/common/ErrorBoundary';
 
 // Register Service Worker for PWA & Background Sync
