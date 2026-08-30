18b8fe01a7fb326b67b74d0948e412e3860367d5
Yoseph-M <zubease22@gmail.com>
2026-08-30 23:55:01 +0300

    feat: add header title integration to manager expenses

--- frontend/src/pages/manager/ManagerExpenses.tsx
+++ frontend/src/pages/manager/ManagerExpenses.tsx
@@ -1,4 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ExpensesTracker } from '../../components/common/ExpensesTracker';
+import { useHeaderStore } from '../../store/headerStore';
 
-export const ManagerExpenses: React.FC = () => <ExpensesTracker />;
+export const ManagerExpenses: React.FC = () => {
+  const { setPageTitle, setShowDateRange } = useHeaderStore();
+  useEffect(() => {
+    setPageTitle({ title: 'Expenses', subtitle: 'Track operational spend' });
+    setShowDateRange(false);
+    return () => {
+      setPageTitle({ title: 'Overview', subtitle: '' });
+      setShowDateRange(false);
+    };
+  }, [setPageTitle, setShowDateRange]);
+  return <ExpensesTracker />;
+};
