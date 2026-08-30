aed19e0d7d2bf2b5e21898f30f088ffff7acbdd8
Yoseph-M <zubease22@gmail.com>
2026-08-30 23:55:15 +0300

    feat: add header title integration to owner expenses

--- frontend/src/pages/owner/OwnerExpenses.tsx
+++ frontend/src/pages/owner/OwnerExpenses.tsx
@@ -1,4 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ExpensesTracker } from '../../components/common/ExpensesTracker';
+import { useHeaderStore } from '../../store/headerStore';
 
-export const OwnerExpenses: React.FC = () => <ExpensesTracker />;
+export const OwnerExpenses: React.FC = () => {
+  const { setPageTitle, setShowDateRange } = useHeaderStore();
+  useEffect(() => {
+    setPageTitle({ title: 'Expenses', subtitle: 'Track operational spend' });
+    setShowDateRange(false);
+    return () => {
+      setPageTitle({ title: 'Overview', subtitle: '' });
+      setShowDateRange(false);
+    };
+  }, [setPageTitle, setShowDateRange]);
+  return <ExpensesTracker />;
+};
